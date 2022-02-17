0997ebf5e1d461a1cd8b9c40d6f5c6fc4110b476
ngandriau <ngandriau@protonmail.com>
2022-02-17 07:03:32 +0100

    change version of the app to '0.1-beta'

--- livres/apps.py
+++ livres/apps.py
@@ -4,4 +4,4 @@ from django.apps import AppConfig
 class LivresConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'livres'
-    version = '0.0.1'
+    version = '0.1-beta'
